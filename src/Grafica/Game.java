f75f9e41fea87349e5c0abe94a72e3903879d820
SamuUNIBG <s.vecchi10@studenti.com>
2024-04-29 12:50:48 +0200

    aggiunge HashMap immagini carte

--- src/Grafica/Game.java
+++ src/Grafica/Game.java
@@ -4,15 +4,22 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 
 //utilizzare JTabbedPane. Guardare https://www.edatlas.it/scarica/informatica/info_java/Inserto3/5ScrollPaneTabbedPane.pdf
 
 public class Game extends JFrame implements ActionListener{
+	
+	HashMap<Integer, String> cardImage = new HashMap<Integer, String>();
 
 	public Game(ArrayList<String> username, ArrayList<String> userColor) {
+		
+		createHshMap();
+		
 		System.out.println(username.get(0));
 		System.out.println(username.get(1));
 		System.out.println(username.get(2));
@@ -33,6 +40,138 @@ public class Game extends JFrame implements ActionListener{
 		scrollPlayingField.getHorizontalScrollBar().setValue((12500-204)/2);
 		scrollPlayingField.getVerticalScrollBar().setValue((5000-142)/2);
 		
+	}
+	
+	private void createHshMap() {
+		//front
+		//add resource card to HashMap
+		cardImage.put(0, "images/cards_front/blue_resource_card_front_1.jpg");
+		cardImage.put(1, "images/cards_front/blue_resource_card_front_2.jpg");
+		cardImage.put(2, "images/cards_front/blue_resource_card_front_3.jpg");
+		cardImage.put(3, "images/cards_front/blue_resource_card_front_4.jpg");
+		cardImage.put(4, "images/cards_front/blue_resource_card_front_5.jpg");
+		cardImage.put(5, "images/cards_front/blue_resource_card_front_6.jpg");
+		cardImage.put(6, "images/cards_front/blue_resource_card_front_7.jpg");
+		cardImage.put(7, "images/cards_front/blue_resource_card_front_8.jpg");
+		cardImage.put(8, "images/cards_front/blue_resource_card_front_9.jpg");
+		cardImage.put(9, "images/cards_front/blue_resource_card_front_10.jpg");
+		cardImage.put(10, "images/cards_front/green_resource_card_front_1.jpg");
+		cardImage.put(11, "images/cards_front/green_resource_card_front_2.jpg");
+		cardImage.put(12, "images/cards_front/green_resource_card_front_3.jpg");
+		cardImage.put(13, "images/cards_front/green_resource_card_front_4.jpg");
+		cardImage.put(14, "images/cards_front/green_resource_card_front_5.jpg");
+		cardImage.put(15, "images/cards_front/green_resource_card_front_6.jpg");
+		cardImage.put(16, "images/cards_front/green_resource_card_front_7.jpg");
+		cardImage.put(17, "images/cards_front/green_resource_card_front_8.jpg");
+		cardImage.put(18, "images/cards_front/green_resource_card_front_9.jpg");
+		cardImage.put(19, "images/cards_front/green_resource_card_front_10.jpg");
+		cardImage.put(20, "images/cards_front/purple_resource_card_front_1.jpg");
+		cardImage.put(21, "images/cards_front/purple_resource_card_front_2.jpg");
+		cardImage.put(22, "images/cards_front/purple_resource_card_front_3.jpg");
+		cardImage.put(23, "images/cards_front/purple_resource_card_front_4.jpg");
+		cardImage.put(24, "images/cards_front/purple_resource_card_front_5.jpg");
+		cardImage.put(25, "images/cards_front/purple_resource_card_front_6.jpg");
+		cardImage.put(26, "images/cards_front/purple_resource_card_front_7.jpg");
+		cardImage.put(27, "images/cards_front/purple_resource_card_front_8.jpg");
+		cardImage.put(28, "images/cards_front/purple_resource_card_front_9.jpg");
+		cardImage.put(29, "images/cards_front/purple_resource_card_front_10.jpg");
+		cardImage.put(30, "images/cards_front/red_resource_card_front_1.jpg");
+		cardImage.put(31, "images/cards_front/red_resource_card_front_2.jpg");
+		cardImage.put(32, "images/cards_front/red_resource_card_front_3.jpg");
+		cardImage.put(33, "images/cards_front/red_resource_card_front_4.jpg");
+		cardImage.put(34, "images/cards_front/red_resource_card_front_5.jpg");
+		cardImage.put(35, "images/cards_front/red_resource_card_front_6.jpg");
+		cardImage.put(36, "images/cards_front/red_resource_card_front_7.jpg");
+		cardImage.put(37, "images/cards_front/red_resource_card_front_8.jpg");
+		cardImage.put(38, "images/cards_front/red_resource_card_front_9.jpg");
+		cardImage.put(39, "images/cards_front/red_resource_card_front_10.jpg");
+		//add gold card to HashMap
+		cardImage.put(40, "images/cards_front/blue_gold_card_front_1.jpg");
+		cardImage.put(41, "images/cards_front/blue_gold_card_front_2.jpg");
+		cardImage.put(42, "images/cards_front/blue_gold_card_front_3.jpg");
+		cardImage.put(43, "images/cards_front/blue_gold_card_front_4.jpg");
+		cardImage.put(44, "images/cards_front/blue_gold_card_front_5.jpg");
+		cardImage.put(45, "images/cards_front/blue_gold_card_front_6.jpg");
+		cardImage.put(46, "images/cards_front/blue_gold_card_front_7.jpg");
+		cardImage.put(47, "images/cards_front/blue_gold_card_front_8.jpg");
+		cardImage.put(48, "images/cards_front/blue_gold_card_front_9.jpg");
+		cardImage.put(49, "images/cards_front/blue_gold_card_front_10.jpg");
+		cardImage.put(50, "images/cards_front/green_gold_card_front_1.jpg");
+		cardImage.put(51, "images/cards_front/green_gold_card_front_2.jpg");
+		cardImage.put(52, "images/cards_front/green_gold_card_front_3.jpg");
+		cardImage.put(53, "images/cards_front/green_gold_card_front_4.jpg");
+		cardImage.put(54, "images/cards_front/green_gold_card_front_5.jpg");
+		cardImage.put(55, "images/cards_front/green_gold_card_front_6.jpg");
+		cardImage.put(56, "images/cards_front/green_gold_card_front_7.jpg");
+		cardImage.put(57, "images/cards_front/green_gold_card_front_8.jpg");
+		cardImage.put(58, "images/cards_front/green_gold_card_front_9.jpg");
+		cardImage.put(59, "images/cards_front/green_gold_card_front_10.jpg");
+		cardImage.put(60, "images/cards_front/purple_gold_card_front_1.jpg");
+		cardImage.put(61, "images/cards_front/purple_gold_card_front_2.jpg");
+		cardImage.put(62, "images/cards_front/purple_gold_card_front_3.jpg");
+		cardImage.put(63, "images/cards_front/purple_gold_card_front_4.jpg");
+		cardImage.put(64, "images/cards_front/purple_gold_card_front_5.jpg");
+		cardImage.put(65, "images/cards_front/purple_gold_card_front_6.jpg");
+		cardImage.put(66, "images/cards_front/purple_gold_card_front_7.jpg");
+		cardImage.put(67, "images/cards_front/purple_gold_card_front_8.jpg");
+		cardImage.put(68, "images/cards_front/purple_gold_card_front_9.jpg");
+		cardImage.put(69, "images/cards_front/purple_gold_card_front_10.jpg");
+		cardImage.put(70, "images/cards_front/red_gold_card_front_1.jpg");
+		cardImage.put(71, "images/cards_front/red_gold_card_front_2.jpg");
+		cardImage.put(72, "images/cards_front/red_gold_card_front_3.jpg");
+		cardImage.put(73, "images/cards_front/red_gold_card_front_4.jpg");
+		cardImage.put(74, "images/cards_front/red_gold_card_front_5.jpg");
+		cardImage.put(75, "images/cards_front/red_gold_card_front_6.jpg");
+		cardImage.put(76, "images/cards_front/red_gold_card_front_7.jpg");
+		cardImage.put(77, "images/cards_front/red_gold_card_front_8.jpg");
+		cardImage.put(78, "images/cards_front/red_gold_card_front_9.jpg");
+		cardImage.put(79, "images/cards_front/red_gold_card_front_10.jpg");
+		//add start card to HashMap
+		cardImage.put(80, "images/cards_front/start_card_front_1.jpg");
+		cardImage.put(81, "images/cards_front/start_card_front_2.jpg");
+		cardImage.put(82, "images/cards_front/start_card_front_3.jpg");
+		cardImage.put(83, "images/cards_front/start_card_front_4.jpg");
+		cardImage.put(84, "images/cards_front/start_card_front_5.jpg");
+		cardImage.put(85, "images/cards_front/start_card_front_6.jpg");
+		//add objective card to HashMap
+		cardImage.put(86, "images/cards_front/stair_goal_card_01.jpg");
+		cardImage.put(87, "images/cards_front/stair_goal_card_02.jpg");
+		cardImage.put(88, "images/cards_front/stair_goal_card_03.jpg");
+		cardImage.put(89, "images/cards_front/stair_goal_card_04.jpg");
+		cardImage.put(90, "images/cards_front/l_goal_card_14.jpg");
+		cardImage.put(91, "images/cards_front/l_goal_card_14.jpg");
+		cardImage.put(92, "images/cards_front/l_goal_card_14.jpg");
+		cardImage.put(93, "images/cards_front/l_goal_card_14.jpg");
+		cardImage.put(94, "images/cards_front/resourse_goal_card_21.jpg");
+		cardImage.put(95, "images/cards_front/resourse_goal_card_22.jpg");
+		cardImage.put(96, "images/cards_front/resourse_goal_card_23.jpg");
+		cardImage.put(97, "images/cards_front/resourse_goal_card_24.jpg");
+		cardImage.put(98, "images/cards_front/object_goal_card_31.jpg");
+		cardImage.put(99, "images/cards_front/object_goal_card_32.jpg");
+		cardImage.put(100, "images/cards_front/object_goal_card_33.jpg");
+		cardImage.put(101, "images/cards_front/object_goal_card_34.jpg");
+		
+		//back
+		//add resource card to HashMap
+		cardImage.put(102, "images/cards_front/blue_resource_card_front_3.jpg");
+		cardImage.put(103, "images/cards_front/blue_resource_card_front_4.jpg");
+		cardImage.put(104, "images/cards_front/blue_resource_card_front_5.jpg");
+		cardImage.put(105, "images/cards_front/blue_resource_card_front_6.jpg");
+		//add gold card to HashMap
+		cardImage.put(106, "images/cards_front/blue_resource_card_front_7.jpg");
+		cardImage.put(107, "images/cards_front/blue_resource_card_front_8.jpg");
+		cardImage.put(108, "images/cards_front/blue_resource_card_front_9.jpg");
+		cardImage.put(109, "images/cards_front/blue_resource_card_front_10.jpg");
+		//add start card to HashMap
+		cardImage.put(110, "images/cards_back/start_card_back_1.jpg");
+		cardImage.put(111, "images/cards_back/start_card_back_2.jpg");
+		cardImage.put(112, "images/cards_back/start_card_back_3.jpg");
+		cardImage.put(113, "images/cards_back/start_card_back_4.jpg");
+		cardImage.put(114, "images/cards_back/start_card_back_5.jpg");
+		cardImage.put(115, "images/cards_back/start_card_back_6.jpg");
+		//add objective card to HashMap
+		cardImage.put(116, "images/cards_back/objective_card_back.jpg");
+				
 	}
 
 	@Override
--- src/Tavolo/MCOro.java
+++ src/Tavolo/MCOro.java
@@ -25,51 +25,51 @@ public class MCOro extends Mazzo {
 	 */
 	public MCOro() {
 		cMazzo=new ArrayList<COro>(40);
-		//red
-		cMazzo.add(cOro0 = new COro(1, Simbolo.PIUMA, Simbolo.FUNGO, Colore.ROSSO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PIUMA, Simbolo.VUOTO, 0, 1, 2, 0));	//2 FUNGO, 1 LUPO
-		cMazzo.add(cOro1 = new COro(1, Simbolo.INCHIOSTRO, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.INCHIOSTRO, Simbolo.VUOTO, Simbolo.ASSENTE, 1, 0, 2, 0));	//2 FUNGO, 1 FOLGIA
-		cMazzo.add(cOro2 = new COro(1, Simbolo.PERGAMENA, Simbolo.FUNGO, Colore.ROSSO, Simbolo.PERGAMENA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 0, 0, 2, 1));	//2 FUNGO, 1 FARFALLA
-		cMazzo.add(cOro3 = new COro(2, Simbolo.ANGOLO, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 1, 3, 0));	//3 FUNGO, 1 LUPO
-		cMazzo.add(cOro4 = new COro(2, Simbolo.ANGOLO, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 1, 0, 3, 0));	//3 FUNGHI, 1 FOLGIA
-		cMazzo.add(cOro5 = new COro(2, Simbolo.ANGOLO, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, 0, 0, 3, 1));	//3 FUNGO, 1 FARFALLA
-		cMazzo.add(cOro6 = new COro(3, Simbolo.ASSENTE, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.INCHIOSTRO, 0, 0, 3, 0));	//3 FUNGO
-		cMazzo.add(cOro7 = new COro(3, Simbolo.ASSENTE, Simbolo.FUNGO, Colore.ROSSO, Simbolo.PIUMA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, 0, 0, 3, 0));	//3 FUNGO
-		cMazzo.add(cOro8 = new COro(3, Simbolo.ASSENTE, Simbolo.FUNGO, Colore.ROSSO, Simbolo.ASSENTE, Simbolo.PERGAMENA, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 0, 3, 0));	//3 FUNGO
-		cMazzo.add(cOro9 = new COro(5, Simbolo.ASSENTE, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.VUOTO, 0, 0, 5, 0));	//5 FUNGO
 		//sky blue
-		cMazzo.add(cOro10 = new COro(1, Simbolo.INCHIOSTRO, Simbolo.LUPO, Colore.AZZURRO, Simbolo.INCHIOSTRO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 0, 2, 0, 1));	//2 LUPO, 1 FARFALLA
-		cMazzo.add(cOro11 = new COro(1, Simbolo.PERGAMENA, Simbolo.LUPO, Colore.AZZURRO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PERGAMENA, Simbolo.VUOTO, 1, 2, 0, 0));	//2 LUPO, 1 FOGlIA
-		cMazzo.add(cOro12 = new COro(1, Simbolo.PIUMA, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PIUMA, 0, 2, 1, 0));	//2 LUPO, 1 FUNGO
-		cMazzo.add(cOro13 = new COro(2, Simbolo.ANGOLO, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 3, 0, 1)); 	//3 LUPO, 1 FARFALLA
-		cMazzo.add(cOro14 = new COro(2, Simbolo.ANGOLO, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, 0, 3, 1, 0));	//3 LUPO, 1 FUNGO
-		cMazzo.add(cOro15 = new COro(2, Simbolo.ANGOLO, Simbolo.LUPO, Colore.AZZURRO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, 1, 3, 0, 0));	//3 LUPO, 1 FOGLIA
-		cMazzo.add(cOro16 = new COro(3, Simbolo.ASSENTE, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.PERGAMENA, 0, 3, 0, 0));	//3 LUPO
-		cMazzo.add(cOro17 = new COro(3, Simbolo.ASSENTE, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.INCHIOSTRO, Simbolo.ASSENTE, Simbolo.ASSENTE, 0, 3, 0, 0));	//3 LUPO
-		cMazzo.add(cOro18 = new COro(3, Simbolo.ASSENTE, Simbolo.LUPO, Colore.AZZURRO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PIUMA, Simbolo.ASSENTE, 0, 3, 0, 0));	//3 LUPO
-		cMazzo.add(cOro19 = new COro(5, Simbolo.ASSENTE, Simbolo.LUPO, Colore.AZZURRO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 5, 0, 0));	//5 LUPO
+		cMazzo.add(cOro0 = new COro(1, Simbolo.INCHIOSTRO, Simbolo.LUPO, Colore.AZZURRO, Simbolo.INCHIOSTRO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 0, 2, 0, 1));	//2 LUPO, 1 FARFALLA
+		cMazzo.add(cOro1 = new COro(1, Simbolo.PERGAMENA, Simbolo.LUPO, Colore.AZZURRO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PERGAMENA, Simbolo.VUOTO, 1, 2, 0, 0));	//2 LUPO, 1 FOGlIA
+		cMazzo.add(cOro2 = new COro(1, Simbolo.PIUMA, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PIUMA, 0, 2, 1, 0));	//2 LUPO, 1 FUNGO
+		cMazzo.add(cOro3 = new COro(2, Simbolo.ANGOLO, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 3, 0, 1)); 	//3 LUPO, 1 FARFALLA
+		cMazzo.add(cOro4 = new COro(2, Simbolo.ANGOLO, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, 0, 3, 1, 0));	//3 LUPO, 1 FUNGO
+		cMazzo.add(cOro5 = new COro(2, Simbolo.ANGOLO, Simbolo.LUPO, Colore.AZZURRO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, 1, 3, 0, 0));	//3 LUPO, 1 FOGLIA
+		cMazzo.add(cOro6 = new COro(3, Simbolo.ASSENTE, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.PERGAMENA, 0, 3, 0, 0));	//3 LUPO
+		cMazzo.add(cOro7 = new COro(3, Simbolo.ASSENTE, Simbolo.LUPO, Colore.AZZURRO, Simbolo.VUOTO, Simbolo.INCHIOSTRO, Simbolo.ASSENTE, Simbolo.ASSENTE, 0, 3, 0, 0));	//3 LUPO
+		cMazzo.add(cOro8 = new COro(3, Simbolo.ASSENTE, Simbolo.LUPO, Colore.AZZURRO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PIUMA, Simbolo.ASSENTE, 0, 3, 0, 0));	//3 LUPO
+		cMazzo.add(cOro9 = new COro(5, Simbolo.ASSENTE, Simbolo.LUPO, Colore.AZZURRO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 5, 0, 0));	//5 LUPO
 		//green
-		cMazzo.add(cOro20 = new COro(1, Simbolo.PIUMA, Simbolo.FOGLIA, Colore.VERDE, Simbolo.PIUMA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 2, 0, 0, 1));	//2 FOGLIA, 1 FARFALLA
-		cMazzo.add(cOro21 = new COro(1, Simbolo.PERGAMENA, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.PERGAMENA, Simbolo.VUOTO, Simbolo.ASSENTE, 2, 0, 1, 0));	//2 FOLGIA, 1 FUNGO
-		cMazzo.add(cOro22 = new COro(1, Simbolo.INCHIOSTRO, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.INCHIOSTRO, 2, 1, 0, 0));	//2 FOGLIA, 1 LUPO
-		cMazzo.add(cOro23 = new COro(2, Simbolo.ANGOLO, Simbolo.FOGLIA, Colore.VERDE, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, 3, 0, 0, 1));	//3 FOGLIA, 1 FARFALLA
-		cMazzo.add(cOro24 = new COro(2, Simbolo.ANGOLO, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 3, 1, 0, 0));	//3 FOGLIE, 1 LUPO
-		cMazzo.add(cOro25 = new COro(2, Simbolo.ANGOLO, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, 3, 0, 1, 0));	//3 FOGLIA, 1 FUNGO
-		cMazzo.add(cOro26 = new COro(3, Simbolo.ASSENTE, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.PIUMA, 3, 0, 0, 0));	//3 FOGLIA
-		cMazzo.add(cOro27 = new COro(3, Simbolo.ASSENTE, Simbolo.FOGLIA, Colore.VERDE, Simbolo.PERGAMENA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, 3, 0, 0, 0));	//3 FOGLIA
-		cMazzo.add(cOro28 = new COro(3, Simbolo.ASSENTE, Simbolo.FOGLIA, Colore.VERDE, Simbolo.ASSENTE, Simbolo.INCHIOSTRO, Simbolo.VUOTO, Simbolo.ASSENTE, 3, 0, 0, 0));	//3 FOGLIE
-		cMazzo.add(cOro29 = new COro(5, Simbolo.ASSENTE, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, 5, 0, 0, 0));	//5 fOGLIA
+		cMazzo.add(cOro10 = new COro(1, Simbolo.PIUMA, Simbolo.FOGLIA, Colore.VERDE, Simbolo.PIUMA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 2, 0, 0, 1));	//2 FOGLIA, 1 FARFALLA
+		cMazzo.add(cOro11 = new COro(1, Simbolo.PERGAMENA, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.PERGAMENA, Simbolo.VUOTO, Simbolo.ASSENTE, 2, 0, 1, 0));	//2 FOLGIA, 1 FUNGO
+		cMazzo.add(cOro12 = new COro(1, Simbolo.INCHIOSTRO, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.INCHIOSTRO, 2, 1, 0, 0));	//2 FOGLIA, 1 LUPO
+		cMazzo.add(cOro13 = new COro(2, Simbolo.ANGOLO, Simbolo.FOGLIA, Colore.VERDE, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, 3, 0, 0, 1));	//3 FOGLIA, 1 FARFALLA
+		cMazzo.add(cOro14 = new COro(2, Simbolo.ANGOLO, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 3, 1, 0, 0));	//3 FOGLIE, 1 LUPO
+		cMazzo.add(cOro15 = new COro(2, Simbolo.ANGOLO, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, 3, 0, 1, 0));	//3 FOGLIA, 1 FUNGO
+		cMazzo.add(cOro16 = new COro(3, Simbolo.ASSENTE, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.PIUMA, 3, 0, 0, 0));	//3 FOGLIA
+		cMazzo.add(cOro17 = new COro(3, Simbolo.ASSENTE, Simbolo.FOGLIA, Colore.VERDE, Simbolo.PERGAMENA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, 3, 0, 0, 0));	//3 FOGLIA
+		cMazzo.add(cOro18 = new COro(3, Simbolo.ASSENTE, Simbolo.FOGLIA, Colore.VERDE, Simbolo.ASSENTE, Simbolo.INCHIOSTRO, Simbolo.VUOTO, Simbolo.ASSENTE, 3, 0, 0, 0));	//3 FOGLIE
+		cMazzo.add(cOro19 = new COro(5, Simbolo.ASSENTE, Simbolo.FOGLIA, Colore.VERDE, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, 5, 0, 0, 0));	//5 fOGLIA
 		//purple
-		cMazzo.add(cOro30 = new COro(1, Simbolo.PIUMA, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.PIUMA, Simbolo.VUOTO, Simbolo.ASSENTE, 1, 0, 0, 2));	//2 FARFALLA, 1 FOLGIA
-		cMazzo.add(cOro31 = new COro(1, Simbolo.PERGAMENA, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PERGAMENA, 0, 1, 0, 2));	//2 FARFALLE, 1 LUPO
-		cMazzo.add(cOro32 = new COro(1, Simbolo.INCHIOSTRO, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.INCHIOSTRO, Simbolo.VUOTO, 0, 0, 1, 2));	//2 FARFALLA, 1 FUNGO
-		cMazzo.add(cOro33 = new COro(2, Simbolo.ANGOLO, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 1, 0, 3));	//3 FARFALLA, 1 LUPO
-		cMazzo.add(cOro34 = new COro(2, Simbolo.ANGOLO, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 1, 0, 0, 3));	//3 FARFALLA, 1 FOGLIA
-		cMazzo.add(cOro35 = new COro(2, Simbolo.ANGOLO, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, 0, 0, 1, 3));	//3 FARFALLA, 1 FUNGO
-		cMazzo.add(cOro36 = new COro(3, Simbolo.ASSENTE, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.INCHIOSTRO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.VUOTO, 0, 0, 0, 3));	//3 FARFALLA
-		cMazzo.add(cOro37 = new COro(3, Simbolo.ASSENTE, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.PERGAMENA, Simbolo.ASSENTE, Simbolo.ASSENTE, 0, 0, 0, 3));	//3 FARFALLA
-		cMazzo.add(cOro38 = new COro(3, Simbolo.ASSENTE, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PIUMA, 0, 0, 0, 3));	//3 FARFALLA
-		cMazzo.add(cOro39 = new COro(5, Simbolo.ASSENTE, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, 0, 0, 0, 5));	//5 FARFALLA
-				
+		cMazzo.add(cOro20 = new COro(1, Simbolo.PIUMA, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.PIUMA, Simbolo.VUOTO, Simbolo.ASSENTE, 1, 0, 0, 2));	//2 FARFALLA, 1 FOLGIA
+		cMazzo.add(cOro21 = new COro(1, Simbolo.PERGAMENA, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PERGAMENA, 0, 1, 0, 2));	//2 FARFALLE, 1 LUPO
+		cMazzo.add(cOro22 = new COro(1, Simbolo.INCHIOSTRO, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.INCHIOSTRO, Simbolo.VUOTO, 0, 0, 1, 2));	//2 FARFALLA, 1 FUNGO
+		cMazzo.add(cOro23 = new COro(2, Simbolo.ANGOLO, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 1, 0, 3));	//3 FARFALLA, 1 LUPO
+		cMazzo.add(cOro4 = new COro(2, Simbolo.ANGOLO, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 1, 0, 0, 3));	//3 FARFALLA, 1 FOGLIA
+		cMazzo.add(cOro25 = new COro(2, Simbolo.ANGOLO, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, 0, 0, 1, 3));	//3 FARFALLA, 1 FUNGO
+		cMazzo.add(cOro26 = new COro(3, Simbolo.ASSENTE, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.INCHIOSTRO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.VUOTO, 0, 0, 0, 3));	//3 FARFALLA
+		cMazzo.add(cOro27 = new COro(3, Simbolo.ASSENTE, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.PERGAMENA, Simbolo.ASSENTE, Simbolo.ASSENTE, 0, 0, 0, 3));	//3 FARFALLA
+		cMazzo.add(cOro28 = new COro(3, Simbolo.ASSENTE, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PIUMA, 0, 0, 0, 3));	//3 FARFALLA
+		cMazzo.add(cOro29 = new COro(5, Simbolo.ASSENTE, Simbolo.FARFALLA, Colore.VIOLA, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, 0, 0, 0, 5));	//5 FARFALLA
+		//red
+		cMazzo.add(cOro30 = new COro(1, Simbolo.PIUMA, Simbolo.FUNGO, Colore.ROSSO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.PIUMA, Simbolo.VUOTO, 0, 1, 2, 0));	//2 FUNGO, 1 LUPO
+		cMazzo.add(cOro31 = new COro(1, Simbolo.INCHIOSTRO, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.INCHIOSTRO, Simbolo.VUOTO, Simbolo.ASSENTE, 1, 0, 2, 0));	//2 FUNGO, 1 FOLGIA
+		cMazzo.add(cOro32 = new COro(1, Simbolo.PERGAMENA, Simbolo.FUNGO, Colore.ROSSO, Simbolo.PERGAMENA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 0, 0, 2, 1));	//2 FUNGO, 1 FARFALLA
+		cMazzo.add(cOro33 = new COro(2, Simbolo.ANGOLO, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 1, 3, 0));	//3 FUNGO, 1 LUPO
+		cMazzo.add(cOro34 = new COro(2, Simbolo.ANGOLO, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, 1, 0, 3, 0));	//3 FUNGHI, 1 FOLGIA
+		cMazzo.add(cOro35 = new COro(2, Simbolo.ANGOLO, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.VUOTO, Simbolo.VUOTO, 0, 0, 3, 1));	//3 FUNGO, 1 FARFALLA
+		cMazzo.add(cOro36 = new COro(3, Simbolo.ASSENTE, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.INCHIOSTRO, 0, 0, 3, 0));	//3 FUNGO
+		cMazzo.add(cOro37 = new COro(3, Simbolo.ASSENTE, Simbolo.FUNGO, Colore.ROSSO, Simbolo.PIUMA, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, 0, 0, 3, 0));	//3 FUNGO
+		cMazzo.add(cOro38 = new COro(3, Simbolo.ASSENTE, Simbolo.FUNGO, Colore.ROSSO, Simbolo.ASSENTE, Simbolo.PERGAMENA, Simbolo.VUOTO, Simbolo.ASSENTE, 0, 0, 3, 0));	//3 FUNGO
+		cMazzo.add(cOro39 = new COro(5, Simbolo.ASSENTE, Simbolo.FUNGO, Colore.ROSSO, Simbolo.VUOTO, Simbolo.ASSENTE, Simbolo.ASSENTE, Simbolo.VUOTO, 0, 0, 5, 0));	//5 FUNGO
+					
 		Collections.shuffle(cMazzo);
 	}
 	
