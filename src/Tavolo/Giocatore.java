a3617d519572c74d441d9b2f5b25022e56855dd9
SamuUNIBG <s.vecchi10@studenti.com>
2024-05-07 22:09:16 +0200

    sposta metodi giocaCIniz e giocaC dalla classe Giocatore alla classe LogicGioco

--- src/Tavolo/Giocatore.java
+++ src/Tavolo/Giocatore.java
@@ -24,6 +24,7 @@ public class Giocatore {
 	private CampoGioco campo;
 	private int[] risPossedute;
 	private int[] oggPosseduti;
+	private ArrayList<String> cPiazzate;
 	
 	public Giocatore(String soprannome,Colore colore,boolean primo) {
 		this.soprannome=soprannome;
@@ -33,6 +34,9 @@ public class Giocatore {
 		campo = new CampoGioco();
 		
 		cMano = new ArrayList<Carta>();
+		cPiazzate =  new ArrayList<String>();
+		cPiazzate.add("999 -> cella vuota");
+
 		
 		cObbPer = null;
 		cInizPer = null;
@@ -44,7 +48,6 @@ public class Giocatore {
 			risPossedute[i]=0;
 			if(i<3) {
 				oggPosseduti[i]=0;
-				cMano.add(null);
 			}
 			
 		}
@@ -62,127 +65,18 @@ public class Giocatore {
 		punteggio+=punti;
 	}
 	public String getSoprannome() {
-		return soprannome;
+		System.out.println(this.soprannome);
+		return this.soprannome;
 	}
 	
-	public boolean giocaCIniz() {
-		
-		int retro;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("La carta iniziale in tuo possesso");
-		System.out.println(cInizPer.toString());
-		
-		do {
-			System.out.print("Vuoi giocare la carta sul retro? [0(si) - 1(no)]: ");
-			retro = sc.nextInt();
-		}while(retro!=0 && retro!=1);
-		
-		sc.close();
-		
-		//se la carta � stata giocata sul retro si setta l'attributo fronte=false
-		if(retro==0) {
-			cInizPer.retro();
-		}
-		
-		PiazzaC("40,40", cInizPer);
-		
-		return true;
-		
+	/**
+	 * @return the cPiazzate
+	 */
+	public ArrayList<String> getCPiazzate() {
+		return cPiazzate;
 	}
 	
-	public boolean giocaC() {
-		
-		Scanner sc = new Scanner(System.in);
-		int numCarta, posY, posX, retro = 0;
-		String posCarta;
-		boolean posCartaOk=false, preRequisito=false;
-		Carta carta;
-		
-		//stampare legenda
-		
-		//viene mostrato a schermo il campo di gioco del giocatore e le sue carte
-		System.out.println("Il tuo campo di gioco:");
-		campo.print();
-		System.out.println("Le carte in tuo possesso:");
-		System.out.println("0) " + cObbPer.toString());
-		for(int i=0; i<3; i++) {
-			System.out.println((i+1) + ") " + cMano.get(i).toString());
-		}
-		
-		do {
-			//il giocatore sceglie quale carta giocare e su che faccia
-			do {
-				System.out.print("Inserire il numero relativo alla carta da giocare: ");
-				numCarta = sc.nextInt();
-			}while(numCarta<1 || numCarta>3);			
-
-			//controllo giocabilit� carta oro
-			if(cMano.get(numCarta) instanceof COro)
-				if(cMano.get(numCarta).VerificaPrerequisito(this))
-			//controllo giocabilit� carta oro
-			if(cMano.get(numCarta) instanceof COro)
-				if(cMano.get(numCarta).VerificaPrerequisito(this))
-			do {
-				System.out.print("Vuoi giocare la carta sul retro? [0(si) - 1(no)]: ");
-				retro = sc.nextInt();
-			}while(retro!=0 && retro!=1);
-			
-			carta = cMano.get(numCarta);
-			
-			//controllo giocabilit� carta oro
-			if(carta instanceof COro && retro==1)
-				if(carta.VerificaPrerequisito(this))
-					preRequisito = true;
-		}while(!preRequisito);
-		
-		//il giocatore sceglie dove giocare la carta
-		System.out.println("Posizioni (y,x) dove e' possibile posizionare la carta:");
-		for(int i=0; i<campo.getPosizioniDisponibili().size(); i++) {
-			System.out.println(campo.getPosizioniDisponibili());
-		}
-		
-		do {
-			do {
-				System.out.print("Inserire le coordinate y di dove si vuole giocare la carta: ");
-				posY = sc.nextInt();
-			}while(posY<0 || posY>CampoGioco.dimensioneY);
-			do {
-				System.out.print("Inserire le coordinate x di dove si vuole giocare la carta: ");
-				posX = sc.nextInt();
-			}while(posX<0 || posX>CampoGioco.dimensioneX);
-			posCarta = posY + "," + posX;
-			for(int i=0; i<campo.getPosizioniDisponibili().size(); i++) {
-				if(posCarta.equals(campo.getPosizioniDisponibili().get(i)))
-					posCartaOk = true;
-			}
-		}while(!posCartaOk);
-		
-		sc.close();
-		
-		//se la carta e' stata giocata sul retro si crea una nuova carta
-		if(retro==0) {
-			if(carta instanceof CRis) {
-				CRis cartaRetro= new CRis(carta.getSimbolo(), carta.getColore(), carta.getIDCARTA());
-				cMano.set(numCarta, cartaRetro);
-			}else if(carta instanceof COro) {
-				COro cartaRetro= new COro(carta.getSimbolo(), carta.getColore(), carta.getIDCARTA());
-				cMano.set(numCarta, cartaRetro);
-			}
-		}
-		PiazzaC(posCarta, carta);
-		if(retro==1) {
-			int puntiCarta = carta.getPunti();
-			if(carta instanceof COro)
-				puntiCarta = ((COro)carta).calcolaMiniObb(posCarta,this);
-			this.addPunteggio(puntiCarta);
-		}
-		
-		return true;
-	}
-	
-	private void PiazzaC(String posCarta, Carta carta) {
+	public void piazzaC(String posCarta, Carta carta) {
 		
 		campo.aggiungiC(posCarta, carta);
 		campo.print(posCarta, carta);
@@ -257,6 +151,10 @@ public class Giocatore {
 			cInizPer = (CIniz)carta;
 		}
 		
+	}
+
+	public ArrayList<Carta> getCMano() {
+		return cMano;
 	}
 
 }
